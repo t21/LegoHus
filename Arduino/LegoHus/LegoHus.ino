01bc1c4bbacccc752f9202fdb2e49cb19abf68b4
t21 <thomas@iMac.local>
2016-08-02 14:06:07 +0200

    Fully functional again

--- Arduino/LegoHus/LegoHus.ino
+++ Arduino/LegoHus/LegoHus.ino
@@ -1,8 +1,9 @@
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+#include <math.h>
 
-#define ENABLE_DEBUG_PRINTS
+//#define ENABLE_DEBUG_PRINTS
 
 /****************************** Pins ******************************************/
 const uint8_t BLUE_LED = D0;  // GPIO16, Onboard blue LED
@@ -46,10 +47,10 @@ Adafruit_MQTT_Publish led7ValuePub = Adafruit_MQTT_Publish(&mqtt, LED7_VALUE_FEE
 const char WILL_FEED[] PROGMEM = AIO_USERNAME "/feeds/disconnect";
 
 const char ERROR_FEED[] PROGMEM = AIO_USERNAME "/errors";
-Adafruit_MQTT_Subscribe errors = Adafruit_MQTT_Subscribe(&mqtt, ERROR_FEED);
+Adafruit_MQTT_Subscribe errorFeed = Adafruit_MQTT_Subscribe(&mqtt, ERROR_FEED);
 
 const char THROTTLE_FEED[] PROGMEM = AIO_USERNAME "/throttle";
-Adafruit_MQTT_Subscribe throttle = Adafruit_MQTT_Subscribe(&mqtt, THROTTLE_FEED);
+Adafruit_MQTT_Subscribe throttleFeed = Adafruit_MQTT_Subscribe(&mqtt, THROTTLE_FEED);
 
 /************************* Global variables *********************************/
 typedef enum {
@@ -60,21 +61,34 @@ typedef enum {
 
 state_t currentState = STATE_NONE;
 
+const double MY_LATITUDE = 55.70584;
+const double MY_LONGITUDE = 13.19321;
 
 /**
  * 
  */
 void timeCallback(uint32_t timeNow) {
 
-    Serial.println(timeNow);
-    Serial.println(getDayOfYear(timeNow));
-    Serial.print("Sunrise:");
-    Serial.println(getSunriseTime(timeNow));
-    Serial.print("Sunset:");
-    Serial.println(getSunsetTime(timeNow));
+    #ifdef ENABLE_DEBUG_PRINTS
+        Serial.print(timeNow);
+        Serial.print(" ");
+        debugPrintTime(timeNow);
+        Serial.print("SunRise:"); 
+        Serial.print(calculateSunrise(timeNow, MY_LATITUDE, MY_LONGITUDE, 0, 0));
+        Serial.print(" ");
+        debugPrintTime(calculateSunrise(timeNow, MY_LATITUDE, MY_LONGITUDE, 0, 0));
+        Serial.print("SunSet:"); 
+        Serial.print(calculateSunset(timeNow, MY_LATITUDE, MY_LONGITUDE, 0, 0));
+        Serial.print(" ");
+        debugPrintTime(calculateSunset(timeNow, MY_LATITUDE, MY_LONGITUDE, 0, 0));
+    #endif
     
-    if ((timeNow > getSunriseTime(timeNow)) && (timeNow < getSunsetTime(timeNow))) {
-        Serial.println("Sun is up");
+    if ((timeNow > calculateSunrise(timeNow, MY_LATITUDE, MY_LONGITUDE, 0, 0)) && 
+        (timeNow < calculateSunset(timeNow, MY_LATITUDE, MY_LONGITUDE, 0, 0))) 
+    {
+        #ifdef ENABLE_DEBUG_PRINTS
+            Serial.println("Sun is up");
+        #endif
         if (currentState == STATE_NONE) {
             currentState = STATE_SUN_UP;
             Serial.println("Sending lights off ...");
@@ -89,7 +103,9 @@ void timeCallback(uint32_t timeNow) {
             }
         }
     } else {
-        Serial.println("Sun is down");
+        #ifdef ENABLE_DEBUG_PRINTS
+            Serial.println("Sun is down");
+        #endif
         if (currentState == STATE_NONE) {
             currentState = STATE_SUN_DOWN;
             Serial.println("Sending lights on ...");
@@ -114,6 +130,9 @@ void timeCallback(uint32_t timeNow) {
 
 
 void allLedsCallback(char *str, uint16_t len) {
+    Serial.print("allLedsCallback: ");
+    Serial.println((char *)str);
+    
     if (strcmp((char *)str, "ON") == 0) {
         digitalWrite(LED1, HIGH);
         digitalWrite(LED2, HIGH);
@@ -177,6 +196,12 @@ void setup()
     led7ValueFeed.setCallback(led7ValueCallback);
     mqtt.subscribe(&led7ValueFeed);
 
+    errorFeed.setCallback(errorCallback);
+    mqtt.subscribe(&errorFeed);
+    
+    throttleFeed.setCallback(throttleCallback);
+    mqtt.subscribe(&throttleFeed);
+
     mqtt.will(FPSTR(WILL_FEED), "LegoHus", 0, 1);
 }
 
@@ -186,28 +211,32 @@ void loop()
     int8_t err_code;
     unsigned long lastPingTime;
 
-    // connect to adafruit io if not connected
-    if (!mqtt.connected()) {
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Wifi connection lost ...");
+        connectWifi();
+    }
+
+    if (mqtt.connected()) {
+        mqtt.processPackets(1000);
+  
+        // Send keep alive
+        if (millis() - lastPingTime > 120000) {
+            lastPingTime = millis();
+            // ping the server to keep the mqtt connection alive
+            if(!mqtt.ping()) {
+                Serial.println("Ping error");
+                mqtt.disconnect();
+            }
+        }
+    } else {
         Serial.print("Connecting to Adafruit IO... ");
         if((err_code = mqtt.connect()) != 0) {
             Serial.println(mqtt.connectErrorString(err_code));
-            while(1);
-        }
-        Serial.println("connected!");
+            delay(60000);
+        } else {
+            Serial.println("connected!");
+        }    
     }
-
-    mqtt.processPackets(1000);
-  
-    // Send keep alive
-    if (millis() - lastPingTime > 120000) {
-        lastPingTime = millis();
-        // ping the server to keep the mqtt connection alive
-        if(!mqtt.ping()) {
-            Serial.println("Ping error");
-            mqtt.disconnect();
-        }
-    }
-
 }
 
 
@@ -216,7 +245,6 @@ void loop()
  */
 void initGpio()
 {
-    // set power switch tail pin as an output
     pinMode(BLUE_LED, OUTPUT);
     digitalWrite(BLUE_LED, HIGH);
     pinMode(LED1, OUTPUT);
@@ -238,21 +266,38 @@ void initWifi()
     
     listNetworks();
 
+//    Serial.print("Attempting to connect to SSID: ");
+//    Serial.println(WLAN_SSID);
+    
+//    WiFi.begin(WLAN_SSID, WLAN_PASS);
+//    while (WiFi.status() != WL_CONNECTED) {
+//        delay(500);
+//        Serial.print(F("."));
+//    }
+//    Serial.println(F(" WiFi connected."));
+
+    connectWifi();
+    
+//    printWifiStatus();    
+}
+
+
+void connectWifi()
+{
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(WLAN_SSID);
     
     WiFi.begin(WLAN_SSID, WLAN_PASS);
+    
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(F("."));
     }
-    Serial.println(F(" WiFi connected."));
+
+    Serial.println();
     
     printWifiStatus();    
 }
-
-
-
 
 
 
@@ -270,7 +315,7 @@ void printDigits(int digits)
 unsigned long getSunriseTime(unsigned long timeNow)
 {
     int dayOfYear;
-    unsigned int startOfDayInSeconds;
+    unsigned long startOfDayInSeconds;
     double timeOfDay;
 
     dayOfYear = getDayOfYear(timeNow);
@@ -292,7 +337,7 @@ unsigned long getSunriseTime(unsigned long timeNow)
 unsigned long getSunsetTime(unsigned long timeNow)
 {
     int dayOfYear;
-    unsigned int startOfDayInSeconds;
+    unsigned long startOfDayInSeconds;
     double timeOfDay;
 
     dayOfYear = getDayOfYear(timeNow);
@@ -339,6 +384,8 @@ int getDayOfYear(unsigned long timeInput)
     days -= LEAP_YEAR(year) ? 366 : 365;
 
     time  -= days; // now it is days in this year, starting at 0
+
+    time++; // Add 1 so days are 1-365
     
     return time;
 }
@@ -479,13 +526,13 @@ void printWifiConnectionStatus()
 }
 
 
-void debugPrintTime(unsigned int timeNow)
+void debugPrintTime(unsigned long timeNow)
 {
-    Serial.println();
-    Serial.println(timeNow);
+//    Serial.println();
+//    Serial.println(timeNow);
     
     // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+//    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
     Serial.print((timeNow  % 86400L) / 3600); // print the hour (86400 equals secs per day)
     Serial.print(':');
     if ( ((timeNow % 3600) / 60) < 10 ) {
@@ -501,3 +548,263 @@ void debugPrintTime(unsigned int timeNow)
     Serial.println(timeNow % 60); // print the second
 }
 
+
+#define ZENITH -.83
+
+unsigned long calculateSunrise(uint32_t timeInSeconds, double lat, double lng, int localOffset, int daylightSavings) {
+    /*
+    localOffset will be <0 for western hemisphere and >0 for eastern hemisphere
+    daylightSavings should be 1 if it is in effect during the summer otherwise it should be 0
+    */
+    //1. first calculate the day of the year
+    double N = getDayOfYear(timeInSeconds);
+    //Serial.print("N:"); Serial.println(N);
+
+    //2. convert the longitude to hour value and calculate an approximate time
+    double lngHour = lng / 15.0;      
+    double t = N + ((6 - lngHour) / 24);   //if rising time is desired:
+    //float t = N + ((18 - lngHour) / 24)   //if setting time is desired:
+
+    //3. calculate the Sun's mean anomaly   
+    double M = (0.9856 * t) - 3.289;
+
+    //4. calculate the Sun's true longitude
+    double L = M + (1.916 * sin((PI/180)*M)) + (0.020 * sin(2 *(PI/180) * M)) + 282.634;
+    //Serial.print("L:"); Serial.println(L);
+    if (L > 360) {
+        L -= 360.0;
+    } else if (L < 0) {
+        L += 360.0;
+    }
+
+    //5a. calculate the Sun's right ascension      
+    double RA = 180/PI*atan(0.91764 * tan((PI/180)*L));
+    //Serial.print("RA:"); Serial.println(RA);
+    if (RA > 360) {
+        RA -= 360.0;
+    } else if (RA < 0) {
+        RA += 360.0;
+    }
+
+    //5b. right ascension value needs to be in the same quadrant as L   
+    double Lquadrant  = floor( L/90) * 90;
+    double RAquadrant = floor(RA/90) * 90;
+    RA = RA + (Lquadrant - RAquadrant);
+
+    //5c. right ascension value needs to be converted into hours   
+    RA = RA / 15;
+
+    //6. calculate the Sun's declination
+    double sinDec = 0.39782 * sin((PI/180)*L);
+    double cosDec = cos(asin(sinDec));
+
+    //7a. calculate the Sun's local hour angle
+    double cosH = (sin((PI/180)*ZENITH) - (sinDec * sin((PI/180)*lat))) / (cosDec * cos((PI/180)*lat));
+    /*   
+    if (cosH >  1) 
+    the sun never rises on this location (on the specified date)
+    if (cosH < -1)
+    the sun never sets on this location (on the specified date)
+    */
+
+    //7b. finish calculating H and convert into hours
+    double H = 360 - (180/PI)*acos(cosH);   //   if if rising time is desired:
+    //float H = acos(cosH) //   if setting time is desired:      
+    H = H / 15;
+
+    //8. calculate local mean time of rising/setting      
+    double T = H + RA - (0.06571 * t) - 6.622;
+
+    //9. adjust back to UTC
+    double UT = T - lngHour;
+    //Serial.print("UT:"); Serial.println(UT);
+    if (UT > 24) {
+        UT -= 24.0;
+    } else if (UT < 0) {
+        UT += 24.0;
+    }
+
+    //10. convert UT value to local time zone of latitude/longitude
+//    return UT + localOffset + daylightSavings;
+    double timeOfDay = UT + localOffset + daylightSavings;
+
+    unsigned long startOfDayInSeconds = getStartOfDayInSeconds(timeInSeconds);
+
+    return (startOfDayInSeconds + timeOfDay * 3600);
+}
+
+
+unsigned long calculateSunset(uint32_t timeInSeconds, double lat, double lng, int localOffset, int daylightSavings) {
+    /*
+    localOffset will be <0 for western hemisphere and >0 for eastern hemisphere
+    daylightSavings should be 1 if it is in effect during the summer otherwise it should be 0
+    */
+    //1. first calculate the day of the year
+    double N = getDayOfYear(timeInSeconds);
+    //Serial.print("N:"); Serial.println(N);
+
+    //2. convert the longitude to hour value and calculate an approximate time
+    double lngHour = lng / 15.0;      
+    //double t = N + ((6 - lngHour) / 24);   //if rising time is desired:
+    double t = N + ((18 - lngHour) / 24);   //if setting time is desired:
+
+    //3. calculate the Sun's mean anomaly   
+    double M = (0.9856 * t) - 3.289;
+
+    //4. calculate the Sun's true longitude
+    double L = M + (1.916 * sin((PI/180)*M)) + (0.020 * sin(2 *(PI/180) * M)) + 282.634;
+    //Serial.print("L:"); Serial.println(L);
+    if (L > 360) {
+        L -= 360.0;
+    } else if (L < 0) {
+        L += 360.0;
+    }
+
+    //5a. calculate the Sun's right ascension      
+    double RA = 180/PI*atan(0.91764 * tan((PI/180)*L));
+    //Serial.print("RA:"); Serial.println(RA);
+    if (RA > 360) {
+        RA -= 360.0;
+    } else if (RA < 0) {
+        RA += 360.0;
+    }
+
+    //5b. right ascension value needs to be in the same quadrant as L   
+    double Lquadrant  = floor( L/90) * 90;
+    double RAquadrant = floor(RA/90) * 90;
+    RA = RA + (Lquadrant - RAquadrant);
+
+    //5c. right ascension value needs to be converted into hours   
+    RA = RA / 15;
+
+    //6. calculate the Sun's declination
+    double sinDec = 0.39782 * sin((PI/180)*L);
+    double cosDec = cos(asin(sinDec));
+
+    //7a. calculate the Sun's local hour angle
+    double cosH = (sin((PI/180)*ZENITH) - (sinDec * sin((PI/180)*lat))) / (cosDec * cos((PI/180)*lat));
+    /*   
+    if (cosH >  1) 
+    the sun never rises on this location (on the specified date)
+    if (cosH < -1)
+    the sun never sets on this location (on the specified date)
+    */
+
+    //7b. finish calculating H and convert into hours
+    //double H = 360 - (180/PI)*acos(cosH);   //   if if rising time is desired:
+    double H = (180/PI)*acos(cosH); //   if setting time is desired:      
+    H = H / 15;
+
+    //8. calculate local mean time of rising/setting      
+    double T = H + RA - (0.06571 * t) - 6.622;
+
+    //9. adjust back to UTC
+    double UT = T - lngHour;
+    //Serial.print("UT:"); Serial.println(UT);
+    if (UT > 24) {
+        UT -= 24.0;
+    } else if (UT < 0) {
+        UT += 24.0;
+    }
+
+    //10. convert UT value to local time zone of latitude/longitude
+//    return UT + localOffset + daylightSavings;
+    double timeOfDay = UT + localOffset + daylightSavings;
+
+    unsigned long startOfDayInSeconds = getStartOfDayInSeconds(timeInSeconds);
+
+    return (startOfDayInSeconds + timeOfDay * 3600);
+}
+
+
+float calculateSunrise2(int year,int month,int day,float lat, float lng,int localOffset, int daylightSavings) {
+    /*
+    localOffset will be <0 for western hemisphere and >0 for eastern hemisphere
+    daylightSavings should be 1 if it is in effect during the summer otherwise it should be 0
+    */
+    //1. first calculate the day of the year
+    float N1 = floor(275 * month / 9);
+    float N2 = floor((month + 9) / 12);
+    float N3 = (1 + floor((year - 4 * floor(year / 4) + 2) / 3));
+    float N = N1 - (N2 * N3) + day - 30;
+    //Serial.print("N:"); Serial.println(N);
+
+    //2. convert the longitude to hour value and calculate an approximate time
+    float lngHour = lng / 15.0;      
+    float t = N + ((6 - lngHour) / 24);   //if rising time is desired:
+    //float t = N + ((18 - lngHour) / 24)   //if setting time is desired:
+
+    //3. calculate the Sun's mean anomaly   
+    float M = (0.9856 * t) - 3.289;
+
+    //4. calculate the Sun's true longitude
+//    float L = fmod(M + (1.916 * sin((PI/180)*M)) + (0.020 * sin(2 *(PI/180) * M)) + 282.634, 360.0);
+    float L = M + (1.916 * sin((PI/180)*M)) + (0.020 * sin(2 *(PI/180) * M)) + 282.634;
+    //Serial.print("L:"); Serial.println(L);
+    if (L > 360) {
+        L -= 360.0;
+    } else if (L < 0) {
+        L += 360.0;
+    }
+
+    //5a. calculate the Sun's right ascension      
+//    float RA = fmod(180/PI*atan(0.91764 * tan((PI/180)*L)), 360.0);
+    float RA = 180/PI*atan(0.91764 * tan((PI/180)*L));
+    //Serial.print("RA:"); Serial.println(RA);
+    if (RA > 360) {
+        RA -= 360.0;
+    } else if (RA < 0) {
+        RA += 360.0;
+    }
+
+    //5b. right ascension value needs to be in the same quadrant as L   
+    float Lquadrant  = floor( L/90) * 90;
+    float RAquadrant = floor(RA/90) * 90;
+    RA = RA + (Lquadrant - RAquadrant);
+
+    //5c. right ascension value needs to be converted into hours   
+    RA = RA / 15;
+
+    //6. calculate the Sun's declination
+    float sinDec = 0.39782 * sin((PI/180)*L);
+    float cosDec = cos(asin(sinDec));
+
+    //7a. calculate the Sun's local hour angle
+    float cosH = (sin((PI/180)*ZENITH) - (sinDec * sin((PI/180)*lat))) / (cosDec * cos((PI/180)*lat));
+    /*   
+    if (cosH >  1) 
+    the sun never rises on this location (on the specified date)
+    if (cosH < -1)
+    the sun never sets on this location (on the specified date)
+    */
+
+    //7b. finish calculating H and convert into hours
+    float H = 360 - (180/PI)*acos(cosH);   //   if if rising time is desired:
+    //float H = acos(cosH) //   if setting time is desired:      
+    H = H / 15;
+
+    //8. calculate local mean time of rising/setting      
+    float T = H + RA - (0.06571 * t) - 6.622;
+
+    //9. adjust back to UTC
+//    float UT = fmod(T - lngHour,24.0);
+    float UT = T - lngHour;
+    //Serial.print("UT:"); Serial.println(UT);
+    if (UT > 24) {
+        UT -= 24.0;
+    } else if (UT < 0) {
+        UT += 24.0;
+    }
+
+    //10. convert UT value to local time zone of latitude/longitude
+    return UT + localOffset + daylightSavings;
+
+}
+
+//void printSunrise() {
+//    float localT = calculateSunrise(/*args*/);
+//    double hours;
+//    float minutes = modf(localT,&hours)*60;
+//    printf("%.0f:%.0f",hours,minutes);
+//}
+    
